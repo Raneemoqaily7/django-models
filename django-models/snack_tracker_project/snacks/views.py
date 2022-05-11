1606fdc2dec036d26e0347b33545f6c37933c283
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-05-12 01:36:41 +0300

    url

--- django-models/snack_tracker_project/snacks/views.py
+++ django-models/snack_tracker_project/snacks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Snack
 
 
@@ -7,4 +7,10 @@ from .models import Snack
 class SnackListView(ListView):
 
     template_name ="snack_list.html"
+    model =Snack
+
+
+class SnackDetailView(DetailView):
+
+    template_name ="snack_detail.html"
     model =Snack
