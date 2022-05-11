b2ec032903acd3921c3a902bc2b13c2bc68e0364
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-05-11 21:22:53 +0300

    django models

---
+++ django-models/snack_tracker_project/snacks/models.py
@@ -0,0 +1,16 @@
+
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Snack(models.Model):
+    name = models.CharField (max_length=64 , null =True , blank =True)
+    purchaser =models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    description=models.CharField(max_length=255)
+    # price =models.IntegerField(null =True)
+
+
+    def __str__(self):
+        return self.name
+
+
+
---
+++ django-models/snack_tracker_project/snacks/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Snack
+
+
+
+class SnackListView(ListView):
+
+    template_name ="snack_list.html"
+    model =Snack
